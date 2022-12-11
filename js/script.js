ceba1c090bb7147d781c5db71457f16106b4a188
Felilpz <luizfelipe.vieira12@gmail.com>
2022-12-11 08:49:22 -0300

    js feito

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-let pegarBinario = window.document.getElementById('bin2dec')
-let pegarDecimal = window.document.getElementById("dec2bin")
+// Binario para decimal
+let saida = document.getElementById('pegarAqui')
 
-function paraDecimal(binario) {
-    let decimal = 0;
-    let i = 0;
-    let restante
-    
-    while(binario != 0) {
-        restante = binario % 10
-        binario = Number.parseInt(binario / 10)
-        
-    }
+function paraDecimal() {
+    let binario = document.getElementById('bin2dec').value
+
+    let decimal = parseInt(binario, 2)
+
+    saida.innerHTML = `<p class="res">${decimal}</p>`
+}
+
+
+// Decimal para binario
+let saida2 = document.getElementById('pegarAqui2')
+
+function paraBinario() {
+    let decimal = Number(window.document.getElementById('dec2bin').value)
+
+    let binario = decimal.toString(2)
+
+    saida2.innerHTML = `<p class="res">${binario}</p>`
 }
